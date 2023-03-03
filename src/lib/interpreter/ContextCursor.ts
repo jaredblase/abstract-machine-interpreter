2e95bd7151246e8df99c8ce94f40f18b49c4ff19
Jared <jaredblase@gmail.com>
2023-03-03 20:56:20 +0800

    add: ContextCursor class

---
+++ src/lib/interpreter/ContextCursor.ts
@@ -0,0 +1,37 @@
+import type { TreeCursor } from '@lezer/common'
+
+export class ContextCursor {
+	#cursor: TreeCursor
+	#src: string
+	#hasRemainingNodes = true
+
+	constructor(cursor: TreeCursor, src: string) {
+		this.#cursor = cursor
+		this.#src = src
+	}
+
+	next() {
+		if (this.#hasRemainingNodes) {
+			this.#hasRemainingNodes = this.#cursor.next()
+		}
+		
+		return this.#hasRemainingNodes
+	}
+
+	isType(type: string) {
+		return this.#cursor.node.type.name === type
+	}
+
+	getToken() {
+		return this.#src.slice(this.#cursor.node.from, this.#cursor.node.to)
+	}
+
+	toString() {
+		return `${this.#cursor.node.type.name}: ${this.getToken()}`
+	}
+
+	getRowColPos() {
+		const arr = this.#src.slice(0, this.#cursor.node.from).split('\n')
+		return `${arr.length}:${arr.at(-1).length}`
+	}
+}
