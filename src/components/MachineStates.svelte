4fd54d44a6ccd0a85aaa99486f96df6e3fe8ceb7
Jared <jaredblase@gmail.com>
2023-03-04 23:50:09 +0800

    add: machine states

---
+++ src/components/MachineStates.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import BoxedLabel from './BoxedLabel.svelte'
+  import { input, idx, output, currState, steps } from '../stores/machine'
+  import Tape from './Tape.svelte'
+</script>
+
+<section class="mb-8 px-4">
+  <div class="container mx-auto space-y-8">
+    <BoxedLabel label="Input">
+      <Tape value={$input} markIdx={$idx} />
+    </BoxedLabel>
+    <BoxedLabel label="Output">
+      <Tape value={$output} />
+    </BoxedLabel>
+    <div class="grid grid-cols-2 gap-x-4">
+      <BoxedLabel label="State">
+        {$currState || '...'}
+      </BoxedLabel>
+      <BoxedLabel label="Steps">
+        {$steps}
+      </BoxedLabel>
+    </div>
+  </div>
+</section>
