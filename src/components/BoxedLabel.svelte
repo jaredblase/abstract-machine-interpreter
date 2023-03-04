77ee734d37ea7a435e650ee3e47a7e4618d25340
Jared <jaredblase@gmail.com>
2023-03-04 23:50:02 +0800

    refactor: make generic boxed label component

---
+++ src/components/BoxedLabel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let className = ''
+
+  export let label: string
+  export { className as class }
+</script>
+
+<div class="rounded-lg border-gray-600 relative border py-3 {className}">
+  <header
+    class="absolute top-0 left-1/2 -translate-y-[60%] -translate-x-1/2 bg-white dark:bg-gray-800 transition-colors px-2 uppercase"
+  >
+    {label}
+  </header>
+  <p class="text-3xl font-mono flex justify-center">
+    <slot />
+  </p>
+</div>
