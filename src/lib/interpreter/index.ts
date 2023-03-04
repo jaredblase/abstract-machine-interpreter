2b7747c06cb8c07481d55f2d710c83a941dc10ca
Jared <jaredblase@gmail.com>
2023-03-04 22:35:54 +0800

    add: interpreter type guards

--- src/lib/interpreter/index.ts
+++ src/lib/interpreter/index.ts
@@ -1,6 +1,7 @@
 export * from './interpreter'
 
-export type InterpreterState = 'RUNNING' | 'PAUSED' | 'INITIALIZED' | 'STEPPING'
+export type MachineMemory = Map<string, Memory>
+export type MachineTransitions = Map<State, Command>
 
 export type Memory = {
 	type: 'STACK' | 'QUEUE' | 'TAPE'
@@ -13,26 +14,48 @@ export type _symbol = string
 export interface Transition {
 	destination: State
 	symbol: _symbol
-	replacement?: _symbol
+}
+
+export interface TapeTransition extends Transition {
+	replacement: _symbol
 }
 
 interface BaseCommand {
 	type: string
+}
+
+export const FSA_COMMANDS = ['SCAN', 'PRINT', 'SCAN RIGHT', 'SCAN LEFT'] as const
+export const PDA_COMMANDS = ['READ', 'WRITE'] as const
+export const TM_COMMANDS = ['RIGHT', 'LEFT', 'UP', 'DOWN'] as const
+
+export interface FSACommand extends BaseCommand {
+	type: typeof FSA_COMMANDS[number]
 	to: Transition[]
 }
 
-export interface FSACommand extends BaseCommand {
-	type: 'SCAN' | 'PRINT' | 'SCAN RIGHT' | 'SCAN LEFT'
-}
-
 export interface PDACommand extends BaseCommand {
-	type: 'READ' | 'WRITE'
+	type: typeof PDA_COMMANDS[number]
 	memoryName: string
+	to: Transition[]
 }
 
 export interface TMCommand extends BaseCommand {
-	type: 'RIGHT' | 'LEFT' | 'UP' | 'DOWN'
+	type: typeof TM_COMMANDS[number]
 	memoryName: string
+	to: TapeTransition[]
 }
 
 export type Command = FSACommand | PDACommand | TMCommand
+
+export function isFSACommand(x: Command): x is FSACommand {
+	return FSA_COMMANDS.find(c => x.type === c) !== undefined
+}
+
+export function isPDACommand(x: Command): x is PDACommand {
+	return PDA_COMMANDS.find(c => x.type === c) !== undefined
+}
+
+export function isTMCommand(x: Command): x is TMCommand {
+	return TM_COMMANDS.find(c => x.type === c) !== undefined
+}
+
