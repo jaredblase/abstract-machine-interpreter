0072bea0648838d639119ca3cf8469b186c0fc21
Jared <jaredblase@gmail.com>
2023-03-04 13:56:30 +0800

    add: global dark deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,css}",
-  ],
+	content: [
+		"./index.html",
+		"./src/**/*.{svelte,css}",
+	],
 	darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('tailwind-scrollbar'),
+		require('tailwindcss-global-dark')
+	],
 }
