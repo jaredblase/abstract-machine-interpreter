6779a2182123efa56fbb1cb8664c851e9490ea25
Jared <jaredblase@gmail.com>
2023-03-06 03:39:39 +0800

    add: default code

--- src/stores/editor.ts
+++ src/stores/editor.ts
@@ -5,5 +5,13 @@ type Message = {
 	type: 'error' | 'info' | ''
 }
 
-export const code = writable('')
+export const code = writable(
+`;; NFA for detecting substring "101"
+
+.LOGIC
+A] SCAN (0, A), (1, A), (1, B)
+B] SCAN (0, C)
+C] SCAN (1, accept)
+`)
+
 export const message = writable<Message>({ text: '', type: '' })
