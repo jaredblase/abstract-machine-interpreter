4fef5caf27ccfb24bd710f021fd91aaf56f871bf
Jared <jaredblase@gmail.com>
2023-03-07 00:14:43 +0800

    add: improve quality of life by allowing submit in input box

--- src/components/ControlPanel.svelte
+++ src/components/ControlPanel.svelte
@@ -1,140 +1,149 @@
 <script lang="ts">
-  import { interpret } from '../lib/interpreter'
-  import { message, code } from '../stores/editor'
-  import { machine } from '../stores/machine'
+	import { interpret } from '../lib/interpreter'
+	import { message, code } from '../stores/editor'
+	import { machine } from '../stores/machine'
 
-  let value = '1000101'
-  let timeout: NodeJS.Timer
-  let idx = 0
+	let value = '1000101'
+	let timeout: NodeJS.Timer
+	let idx = 0
 
-  $: {
-    if (value || $code) {
-      message.set({
-        text: 'Changes have not been saved...',
-        type: '',
-      })
-    }
-  }
+	$: {
+		if (value || $code) {
+			message.set({
+				text: 'Changes have not been saved...',
+				type: '',
+			})
+		}
+	}
 
 	$: if ($machine.acceptedTimeline != undefined) {
 		message.set({
 			type: 'info',
-			text: `Accepted timeline at ${$machine.acceptedTimeline}`
+			text: `Accepted timeline at ${$machine.acceptedTimeline}`,
 		})
 	}
 
-  $: if ($machine.timelines[idx]) {
-    machine.set($machine.timelines[idx])
-  }
+	$: if ($machine.timelines[idx]) {
+		machine.set($machine.timelines[idx])
+	}
 
 	$: isHalted = $machine.isHalted || $machine.isGlobalHalt
 
-  $: isRunning = timeout != null
-  function onReset() {
-    try {
+	$: isRunning = timeout != null
+	function onReset() {
+		try {
 			const m = interpret($code)
-      m.reset(value)
+			m.reset(value)
 			machine.set(m)
 			idx = 0
-      message.set({
-        text: 'Compiled successfully.',
-        type: 'info',
-      })
-    } catch (err) {
-      message.set({
-        text: err.message,
-        type: 'error',
-      })
-    }
-  }
+			message.set({
+				text: 'Compiled successfully.',
+				type: 'info',
+			})
+		} catch (err) {
+			message.set({
+				text: err.message,
+				type: 'error',
+			})
+		}
+	}
 
-  function onStep() {
-    if (!isHalted) {
-      $machine.step()
-      machine.set($machine)
-    } else if (timeout) {
-      clearTimeout(timeout)
-      timeout = null
-    }
-  }
+	function onStep() {
+		if (!isHalted) {
+			$machine.step()
+			machine.set($machine)
+		} else if (timeout) {
+			clearTimeout(timeout)
+			timeout = null
+		}
+	}
 
-  function onPlay() {
-    if (timeout) {
-      clearInterval(timeout)
-      return (timeout = null)
-    }
+	function onPlay() {
+		if (timeout) {
+			clearInterval(timeout)
+			return (timeout = null)
+		}
 
-    timeout = setInterval(onStep, 200)
-  }
+		timeout = setInterval(onStep, 200)
+	}
 </script>
 
-<div>
-  <label for="input" class="block">Initial input</label>
-  <input
-    type="text"
-    id="input"
-    class="text-gray-900 rounded-md py-1 w-full placeholder:text-gray-400 placeholder:italic"
-    placeholder="Input value..."
-    bind:value
-  />
-</div>
-<div class="mt-2">
-  <label for="timeline" class="block">Timeline</label>
-  <select
-    id="timeline"
-    bind:value={idx}
-    disabled={$machine.timelines.length == 0 || isRunning}
-    class="rounded-md py-1 w-full text-gray-900"
-  >
-    {#each $machine.timelines as t (t.id)}
-      <option value={t.id}>
-        {t.id}
-        {#if t.isHalted}({t.isAccepted ? 'accepted' : 'dead'}){/if}
-      </option>
-    {/each}
-  </select>
-</div>
-<p class="text-sm font-semibold min-h-[3rem] mt-1 message {$message.type}">
-  {$message.text}
-</p>
-<div class="grid gap-2 max-md:grid-cols-2">
-  <button
-    class="btn bg-green-600 enabled:hover:bg-green-700 enabled:active:bg-green-800 text-white"
-    disabled={isHalted}
-    on:click={onPlay}>{isRunning ? 'Pause' : 'Play'}</button
-  >
-  <button
-    class="btn default"
-    disabled={isHalted || isRunning}
-    on:click={onStep}>Step</button
-  >
-  <button class="btn default max-md:col-span-2" disabled={isRunning} on:click={onReset}>
-    Reset
-  </button>
-</div>
+<form on:submit|preventDefault={onReset}>
+	<div>
+		<label for="input" class="block">Initial input</label>
+		<input
+			type="text"
+			id="input"
+			class="text-gray-900 rounded-md py-1 w-full placeholder:text-gray-400 placeholder:italic"
+			placeholder="Input value..."
+			bind:value
+		/>
+	</div>
+	<div class="mt-2">
+		<label for="timeline" class="block">Timeline</label>
+		<select
+			id="timeline"
+			bind:value={idx}
+			disabled={$machine.timelines.length == 0 || isRunning}
+			class="rounded-md py-1 w-full text-gray-900"
+		>
+			{#each $machine.timelines as t (t.id)}
+				<option value={t.id}>
+					{t.id}
+					{#if t.isHalted}({t.isAccepted ? 'accepted' : 'dead'}){/if}
+				</option>
+			{/each}
+		</select>
+	</div>
+	<p class="text-sm font-semibold min-h-[3rem] mt-1 message {$message.type}">
+		{$message.text}
+	</p>
+	<div class="grid gap-2 max-md:grid-cols-2">
+		<button
+			type="button"
+			class="btn bg-green-600 enabled:hover:bg-green-700 enabled:active:bg-green-800 text-white"
+			disabled={isHalted}
+			on:click={onPlay}>{isRunning ? 'Pause' : 'Play'}</button
+		>
+		<button
+			type="button"
+			class="btn default"
+			disabled={isHalted || isRunning}
+			on:click={onStep}>Step</button
+		>
+		<button
+			type="submit"
+			class="btn default max-md:col-span-2"
+			disabled={isRunning}
+			on:click={onReset}
+		>
+			Reset
+		</button>
+	</div>
+</form>
 
 <style lang="postcss">
-  .message {
-    @apply text-gray-400;
-  }
+	.message {
+		@apply text-gray-400;
+	}
 
-  .message.info {
-    @apply text-green-600;
-  }
+	.message.info {
+		@apply text-green-600;
+	}
 
-  .message.error {
-    @apply text-red-500;
-  }
+	.message.error {
+		@apply text-red-500;
+	}
 
-  :disabled {
-    @apply cursor-not-allowed opacity-50;
-  }
+	:disabled {
+		@apply cursor-not-allowed opacity-50;
+	}
 
-  .btn {
-    @apply flex-1 rounded-md py-1 transition-colors font-medium;
-  }
+	.btn {
+		@apply flex-1 rounded-md py-1 transition-colors font-medium;
+	}
 
-  .default {
-    @apply bg-gray-100 enabled:hover:bg-gray-200 enabled:active:bg-gray-300 text-gray-800;
-  }
+	.default {
+		@apply bg-gray-100 enabled:hover:bg-gray-200 enabled:active:bg-gray-300 text-gray-800;
+	}
 </style>
