a0d59ef97fadddf5b54cba54fb174e140345711d
Jared <jaredblase@gmail.com>
2023-03-14 16:52:45 +0800

    update: dialog behavior

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -18,8 +18,10 @@
 
 	$: if ($modal == id) {
 		dialog.showModal()
+		document.body.style.overflow = 'hidden'
 	} else {
 		dialog?.close()
+		document.body.style.overflow = 'auto'
 	}
 </script>
 
@@ -27,7 +29,8 @@
 <dialog
 	{id}
 	class="grid place-items-center bg-[unset] w-full h-full open:pointer-events-auto
-		opacity-0 open:opacity-100 pointer-events-none backdrop:bg-black/50 backdrop-blur-[2px]"
+		opacity-0 open:opacity-100 pointer-events-none backdrop:bg-black/50 backdrop-blur-[2px]
+		max-h-full max-w-full p-12"
 	on:click={handleClick}
 	bind:this={dialog}
 >
