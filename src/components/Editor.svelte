82b652e212fa53ee2cfc57b2a89c39faaee3c1f1
Jared <jaredblase@gmail.com>
2023-03-28 22:36:58 +0800

    add: active line color

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -4,14 +4,30 @@
 	import { theme, highlights } from '../lib/absmac/styles'
 	import { AbsMac } from '../lib/absmac'
 	import { onMount } from 'svelte'
+	import { machine } from '../stores/machine'
 
 	let className = ''
 
 	export { className as class }
 
 	onMount(() => {
-		document.querySelector('.cm-content').ariaLabel = "Code editor"
+		document.querySelector('.cm-content').ariaLabel = 'Code editor'
 	})
+
+	let elem: Element | undefined
+	let cachedLineNumber: number
+
+	$: lineNumber = $machine.states.get($machine.currState)?.lineNumber
+
+	$: if (lineNumber !== cachedLineNumber) {
+		cachedLineNumber = lineNumber
+		elem?.classList.remove('active')
+
+		if (lineNumber != undefined) {
+			elem = document.querySelector(`.cm-line:nth-child(${lineNumber})`)
+			elem?.classList.add('active')
+		}
+	}
 </script>
 
 <div class={className}>
@@ -23,3 +39,13 @@
 		placeholder="Enter your code here..."
 	/>
 </div>
+
+<style lang="postcss">
+	:global(.active) {
+		@apply !bg-orange-900 relative;
+	}
+
+	:global(.active::after) {
+		@apply content-["ACTIVE"] italic absolute right-10 opacity-50;
+	}
+</style>
