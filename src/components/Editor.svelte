f83b14e5d078d7c09d56decb4369bf34c6819044
Jared <jaredblase@gmail.com>
2023-03-31 04:20:19 +0800

    add: line highlighting for states in editor

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -5,32 +5,47 @@
 	import { AbsMac } from '../lib/absmac'
 	import { onMount } from 'svelte'
 	import { machine } from '../stores/machine'
+	import type { State } from '../lib/interpreter'
 
 	let className = ''
 
 	export { className as class }
 
 	onMount(() => {
-		document.querySelector('.cm-content').ariaLabel = 'Code editor'
+		editor.querySelector('.cm-content').ariaLabel = 'Code editor'
 	})
 
-	let elem: Element | undefined
-	let cachedLineNumber: number
+	let currElem: Element
+	let prevElem: Element
+	let editor: Element
+	let cachedCurrState: State
+	let cachedPrevState: State
 
-	$: lineNumber = $machine.states.get($machine.currState)?.lineNumber
+	$: currState = $machine.currState
+	$: prevState = $machine.prevState
 
-	$: if (lineNumber !== cachedLineNumber) {
-		cachedLineNumber = lineNumber
-		elem?.classList.remove('active')
+	$: if (editor?.querySelector('.active') == null || currState !== cachedCurrState) {
+		cachedCurrState = currState
+		currElem?.classList.remove('active')
 
-		if (lineNumber != undefined) {
-			elem = document.querySelector(`.cm-line:nth-child(${lineNumber})`)
-			elem?.classList.add('active')
+		if (currState?.lineNumber != undefined) {
+			currElem = editor?.querySelector(`.cm-line:nth-child(${currState.lineNumber})`)
+			currElem?.classList.add('active')
+		}
+	}
+
+	$: if (editor?.querySelector('.prev') == null || prevState !== cachedPrevState) {
+		cachedPrevState = prevState
+		prevElem?.classList.remove('prev')
+
+		if (prevState?.lineNumber != undefined) {
+			prevElem = editor?.querySelector(`.cm-line:nth-child(${prevState.lineNumber})`)
+			prevElem?.classList.add('prev')
 		}
 	}
 </script>
 
-<div class={className}>
+<div class={className} bind:this={editor}>
 	<CodeMirror
 		class="max-h-full"
 		bind:value={$code}
@@ -41,11 +56,35 @@
 </div>
 
 <style lang="postcss">
+	:global(.active, .prev) {
+		@apply relative
+	}
+
+	:global(.active::after, .prev::after) {
+		@apply italic absolute right-10 opacity-50;
+	}
+
 	:global(.active) {
-		@apply !bg-orange-900 relative;
+		@apply !bg-orange-900
 	}
 
 	:global(.active::after) {
-		@apply content-["ACTIVE"] italic absolute right-10 opacity-50;
+		@apply content-["ACTIVE"];
+	}
+
+	:global(.prev) {
+		@apply !bg-fuchsia-900
+	}
+
+	:global(.prev::after) {
+		@apply content-["PREV"];
+	}
+
+	:global(.active.prev) {
+		@apply !bg-rose-900
+	}
+
+	:global(.active.prev::after) {
+		@apply content-["PREV_ACTIVE"]
 	}
 </style>
